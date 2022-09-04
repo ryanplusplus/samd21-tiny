ba4b20572fa0ed99228a6db850414746067c6832
Ryan Hartlage <ryanplusplus@gmail.com>
2022-09-04 15:41:25 -0400

    Switch to SERCOM2 for qt py SPI

--- src/hardware/async_spi_sercom2_pa09_pa10_pa11.c
+++ src/hardware/async_spi_sercom2_pa09_pa10_pa11.c
@@ -6,10 +6,10 @@
 #include "sam.h"
 #include "clock.h"
 #include "dma.h"
-#include "async_spi_sercom0_pa09_pa10_pa11.h"
+#include "async_spi_sercom2_pa09_pa10_pa11.h"
 #include "tiny_utils.h"
 
-#ifdef SERCOM0
+#ifdef SERCOM2
 
 static uint8_t write_channel;
 static uint8_t read_channel;
@@ -34,7 +34,7 @@ static inline void configure_read_channel(const void* buffer, uint16_t buffer_si
   d->BTCTRL.bit.BLOCKACT = DMAC_BTCTRL_BLOCKACT_NOACT_Val;
   d->BTCTRL.bit.VALID = 1;
   d->BTCNT.bit.BTCNT = buffer_size;
-  d->SRCADDR.bit.SRCADDR = (uintptr_t)&SERCOM0->SPI.DATA.reg;
+  d->SRCADDR.bit.SRCADDR = (uintptr_t)&SERCOM2->SPI.DATA.reg;
   d->DESCADDR.bit.DESCADDR = 0;
 
   if(buffer_size > 1) {
@@ -49,7 +49,7 @@ static inline void configure_read_channel(const void* buffer, uint16_t buffer_si
   dma_channel_enable(
     read_channel,
     DMAC_CHCTRLB_TRIGACT_BEAT_Val,
-    SERCOM0_DMAC_ID_RX,
+    SERCOM2_DMAC_ID_RX,
     DMAC_CHCTRLB_LVL_LVL1_Val);
 }
 
@@ -65,13 +65,13 @@ static inline void configure_write_channel(const void* buffer, uint16_t buffer_s
   d->BTCTRL.bit.VALID = 1;
   d->BTCNT.bit.BTCNT = buffer_size;
   d->SRCADDR.bit.SRCADDR = (uintptr_t)buffer + buffer_size;
-  d->DSTADDR.bit.DSTADDR = (uintptr_t)&SERCOM0->SPI.DATA.reg;
+  d->DSTADDR.bit.DSTADDR = (uintptr_t)&SERCOM2->SPI.DATA.reg;
   d->DESCADDR.bit.DESCADDR = 0;
 
   dma_channel_enable(
     write_channel,
     DMAC_CHCTRLB_TRIGACT_BEAT_Val,
-    SERCOM0_DMAC_ID_TX,
+    SERCOM2_DMAC_ID_TX,
     DMAC_CHCTRLB_LVL_LVL0_Val);
 }
 
@@ -104,42 +104,42 @@ static void transfer(
 
 static inline void initialize_peripheral(bool cpol, bool cpha, bool msb_first, uint32_t baud)
 {
-  // Enable SERCOM0 clock
-  PM->APBCMASK.bit.SERCOM0_ = 1;
+  // Enable SERCOM2 clock
+  PM->APBCMASK.bit.SERCOM2_ = 1;
 
   // Select GCLK0 (DFLL48)
   GCLK->CLKCTRL.reg =
     GCLK_CLKCTRL_CLKEN |
     GCLK_CLKCTRL_GEN_GCLK0 |
-    GCLK_CLKCTRL_ID(SERCOM0_GCLK_ID_CORE);
+    GCLK_CLKCTRL_ID(SERCOM2_GCLK_ID_CORE);
   while(GCLK->STATUS.bit.SYNCBUSY) {
   }
 
-  PORT->Group[0].PMUX[9 >> 1].bit.PMUXO = MUX_PA09C_SERCOM0_PAD1;
+  PORT->Group[0].PMUX[9 >> 1].bit.PMUXO = MUX_PA09D_SERCOM2_PAD1;
   PORT->Group[0].PINCFG[9].bit.PMUXEN = 1;
 
-  PORT->Group[0].PMUX[10 >> 1].bit.PMUXE = MUX_PA10C_SERCOM0_PAD2;
+  PORT->Group[0].PMUX[10 >> 1].bit.PMUXE = MUX_PA10D_SERCOM2_PAD2;
   PORT->Group[0].PINCFG[10].bit.PMUXEN = 1;
 
-  PORT->Group[0].PMUX[11 >> 1].bit.PMUXO = MUX_PA11C_SERCOM0_PAD3;
+  PORT->Group[0].PMUX[11 >> 1].bit.PMUXO = MUX_PA11D_SERCOM2_PAD3;
   PORT->Group[0].PINCFG[11].bit.PMUXEN = 1;
 
-  SERCOM0->SPI.CTRLA.reg =
+  SERCOM2->SPI.CTRLA.reg =
     SERCOM_SPI_CTRLA_MODE_SPI_MASTER |
-    SERCOM_SPI_CTRLA_DOPO(1) | // SERCOM0_PAD2, 3 used for MOSI, SCK
-    SERCOM_SPI_CTRLA_DIPO(1) | // SERCOM0_PAD1 used for MISO
+    SERCOM_SPI_CTRLA_DOPO(1) | // SERCOM2_PAD2, 3 used for MOSI, SCK
+    SERCOM_SPI_CTRLA_DIPO(1) | // SERCOM2_PAD1 used for MISO
     (cpha << SERCOM_SPI_CTRLA_CPHA_Pos) |
     (cpol << SERCOM_SPI_CTRLA_CPOL_Pos) |
     (msb_first ? 0 : SERCOM_SPI_CTRLA_DORD);
 
-  SERCOM0->SPI.CTRLB.reg = SERCOM_SPI_CTRLB_RXEN;
-  while(SERCOM0->SPI.SYNCBUSY.bit.CTRLB) {
+  SERCOM2->SPI.CTRLB.reg = SERCOM_SPI_CTRLB_RXEN;
+  while(SERCOM2->SPI.SYNCBUSY.bit.CTRLB) {
   }
 
-  SERCOM0->SPI.BAUD.reg = (clock_gclk0_frequency - 2 * baud) / (2 * baud);
+  SERCOM2->SPI.BAUD.reg = (clock_gclk0_frequency - 2 * baud) / (2 * baud);
 
-  SERCOM0->SPI.CTRLA.bit.ENABLE = 1;
-  while(SERCOM0->SPI.SYNCBUSY.bit.ENABLE) {
+  SERCOM2->SPI.CTRLA.bit.ENABLE = 1;
+  while(SERCOM2->SPI.SYNCBUSY.bit.ENABLE) {
   }
 }
 
@@ -154,7 +154,7 @@ static inline void configure_dma(void)
 
 static const i_tiny_async_spi_api_t api = { transfer };
 
-i_tiny_async_spi_t* async_spi_sercom0_pa09_pa10_pa11_init(bool cpol, bool cpha, bool msb_first, uint32_t baud)
+i_tiny_async_spi_t* async_spi_sercom2_pa09_pa10_pa11_init(bool cpol, bool cpha, bool msb_first, uint32_t baud)
 {
   initialize_peripheral(cpol, cpha, msb_first, baud);
   configure_dma();
